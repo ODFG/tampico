46f6f2aa8d0940538b9421da5ba8ee4c0dc8a5bd
Oscar Flores <odfg90@gmail.com>
2023-09-20 20:22:25 -0600

    Add files via upload

---
+++ LOS DELICIAS DE TAMPICO/data/stock.js
@@ -0,0 +1,68 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Salsa picante El Puplpito 355ml",
+        tipo: "buzo",
+        desc: "Salsa picante para mariscos de Tampico",
+        precio: 50,
+        talle: "M",
+        imagen: 'public/images/PULPITO355ML.webp',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Botana Rancheritos Martinez 250g",
+        tipo: "remera",
+        desc: "Botana de maiz de Tampico",
+        precio: 60,
+        talle: "S",
+        imagen: 'public/images/RANCHERITOS250G.webp',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Refresco Esquis de Hierro 600ml",
+        tipo: "camisa",
+        desc: "Refresco sabor zarzaparrilla de Tampico",
+        precio: 70,
+        talle: "M",
+        imagen: 'public/images/ESCUIS600ML.webp',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Vinage de Manzana L.T.M 1l",
+        tipo: "pantalon",
+        desc: "Vinagre de manzana de Tampico",
+        precio: 100,
+        talle: "S",
+        imagen: 'public/images/VINAGRE5L.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Vainilla La Pureza 1l",
+        tipo: "buzo",
+        desc: "Saborizante vainilla de Tampico",
+        precio: 80,
+        talle: "L",
+        imagen: 'public/images/VAINILLA1L.webp',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Salsa picante Darwings 180ml",
+        tipo: "remera",
+        desc: "Salsa picante agridulce para todo tipo de comida",
+        precio: 50,
+        talle: "L",
+        imagen: 'public/images/DARWINGS180ML.png',
+        cantidad:1
+    }
+];
---
+++ LOS DELICIAS DE TAMPICO/js/getData.js
@@ -0,0 +1,6 @@
+const getProducts = async () => {
+  const response = await fetch('../data/stock.json')
+  const data = await response.json()
+
+  return data
+}
